5ba8e45bb2f75be285639562b76ac75268bb5ff2
Yoseph <yose2369@gmail.com>
2023-03-19 18:57:29 -0400

    Commit from eclipse

---
+++ javaSampleproject/src/commcraft/Example1.java
@@ -0,0 +1,15 @@
+package commcraft;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+		System.out.print("print something");
+
+	}
+
+}
